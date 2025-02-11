ee7f36d4ef6cb58d41dbd2ef796f059d106ebc1e
Tely Lor <tlor1@ewu.edu>
2025-02-11 08:27:29 +0000

    Favicons, fixed breathing, leaves sway

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ console.log('OK');
 
 gsap.to('#Body', {
   
-    scale: 1.1,
+    scale: 1.05,
     duration: 2,
     transformOrigin: '50% 50%',
     repeat: -1,
@@ -13,10 +13,20 @@ gsap.to('#Body', {
 
   gsap.to('#head', {
   
-    scale: 1.1,
+    scale: 1.05,
     duration: 2,
     transformOrigin: '50% 50%',
     repeat: -1,
+    yoyo: true,
+    
+    
+  })
+
+  gsap.to('#leaf', {
+  
+    rotation: -15, transformOrigin:"bottom" ,
+    repeat: -1,
+    duration: 1.5,
     yoyo: true,
     
     
