ec261ceec3ee2f9ad52c4fa7f48a78b3507256f7
Tely Lor <tlor1@ewu.edu>
2025-02-11 02:00:36 +0000

    breathing animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 console.log('OK');
 
+gsap.to('#Body', {
+  
+    scale: 1.1,
+    duration: 2,
+    transformOrigin: '50% 50%',
+    repeat: -1,
+    yoyo: true,
+    
+    
+  })
 
+  gsap.to('#head', {
+  
+    scale: 1.1,
+    duration: 2,
+    transformOrigin: '50% 50%',
+    repeat: -1,
+    yoyo: true,
+    
+    
+  })
