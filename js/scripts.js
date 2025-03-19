775546d816e0c25794510f47505c467109b5ae06
Tely Lor <tlor1@ewu.edu>
2025-03-19 22:00:48 +0000

    remove bamboos. Creates "love u"

--- js/scripts.js
+++ js/scripts.js
@@ -32,4 +32,112 @@ gsap.to('#Body', {
     
   })
 
- 
+  document.getElementById("r1").addEventListener("click", function() {
+    this.style.visibility = "hidden";
+});
+
+document.getElementById("leaf").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r2").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r3").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r4").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r5").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r6").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r7").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r8").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r9").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r10").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r11").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r12").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r13").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r14").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r15").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r16").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r17").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r18").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r19").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("r20").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("leaf").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("leafr").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("leafr2").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("leafr3").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("leafr4").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+document.getElementById("leafr5").addEventListener("click", function() {
+  this.style.visibility = "hidden";
+});
+
+
